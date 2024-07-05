925eecaf44d34ba4837fa7bb266ab75c71910001
wi23b064 <wi23b064@technikum-wien.at>
2024-07-05 15:44:54 +0200

    Update TicTacToe.java

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -3,24 +3,6 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
-    private final Player player1;
-    private final Player player2;
-    private Player currentPlayer;
-    private final Board board;
-    private final Scanner scanner;
-    
-    public TicTacToe(char marker1, char marker2) {
-        player1 = new Player(marker1);
-        player2 = new Player(marker2);
-        currentPlayer = player1;
-        board = new Board();
-        scanner = new Scanner(System.in);
-        
-    }
-    public void switchCurrentPlayer(){
-        currentPlayer = (currentPlayer == player1) ? player2 : player1;
-    }
-
     private boolean hasWinner() {
         return board.checkBoard(currentPlayer.getMarker());
     }
