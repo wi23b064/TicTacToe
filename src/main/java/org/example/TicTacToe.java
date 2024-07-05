15b07229fd431b1a003d8a7e4e401e8a1f7ae682
ts-technikum <wi23b160@technikum-wien.at>
2024-07-05 10:36:50 +0200

    Update TicTacToe.java

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -3,6 +3,11 @@ package org.example;
 public class TicTacToe {
 
     public void switchCurrentPlayer(){
+        if (currentPlayer == player1) {
+            currentPlayer = player2;
+        } else {
+            currentPlayer = player1;
+        }
     }
 
 
@@ -12,19 +17,26 @@ public class TicTacToe {
     private Board board;
 
     public TicTacToe() {
-        
+        player1 = new Player('X');
+        player2 = new Player('O');
+        board = new Board(); //empties board
+        currentPlayer = player1; //default: player1(x) starts
+        scanner = new Scanner(System.in);
     }
 
     public void start() {
-       
+        board.print();
+        System.out.println("Player " + currentPlayer.getMarker() + " wins!");
+        switchCurrentPlayer();
     }
  
     private boolean hasWinner() {
-       
-        return false;
+               return board.checkBoard(currentPlayer.getMarker());
     }
 
     public static void main(String[] args) {
-
+        TicTacToe game = new TicTacToe();
+        game.start();
+    }
     }
 }
