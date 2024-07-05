b24221a28630a70fe546ea0447e2b68288f400fe
ts-technikum <wi23b160@technikum-wien.at>
2024-07-05 10:48:15 +0200

    Update Methods TicTacToe.java

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -8,15 +8,22 @@ public class TicTacToe {
     private Board board;
 
     public TicTacToe() {
-        
+        player1 = new Player('X');
+        player2 = new Player('O');
+        board = new Board(); //empties board
+        currentPlayer = player1; //default: player1(x) starts
+        scanner = new Scanner(System.in);
     }
 
     public void start() {
-       
+       board.print();
+        System.out.println("Player " + currentPlayer.getMarker() + " wins!");
+        switchCurrentPlayer();
     }
 
   public static void main(String[] args) {
-      
+       TicTacToe game = new TicTacToe();
+        game.start();
     }
 
 }
