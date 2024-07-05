f47ee52ed9cbea72f951f9998b475364edada0c4
Selmaazzam <wi23b091@technikum-wien.at>
2024-07-05 09:24:13 +0200

    Place marker in position if cell empty

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -3,6 +3,8 @@ package org.example;
 public class Board {
 
     public void place(int x, int y, char marker){
-        //places marker (x or o) in current position  
+        if (isCellEmpty(x, y)) {
+            cells[x][y] = marker;//if cell empty -> mark spot
+        }  
     }
 }
