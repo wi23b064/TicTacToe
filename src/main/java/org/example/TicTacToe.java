2a7d1048a75d91f18d4f3f489104ad28aaf038e0
Selmaazzam <wi23b091@technikum-wien.at>
2024-07-05 14:34:36 +0200

    Update TicTacToe.java

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -2,28 +2,31 @@ package org.example;
 
 public class TicTacToe {
 
-  private Player player1;
-    private Player player2;
-    private Player currentPlayer;
-    private Board board;
-
-    public TicTacToe() {
-        player1 = new Player('X');
-        player2 = new Player('O');
-        board = new Board(); //empties board
-        currentPlayer = player1; //default: player1(x) starts
-        scanner = new Scanner(System.in);
-    }
-
     public void start() {
-       board.print();
-        System.out.println("Player " + currentPlayer.getMarker() + " wins!");
-        switchCurrentPlayer();
+        boolean start = true;
+        while (start) {
+            board.print();
+            boolean move = makeMove(currentPlayer);
+            if (move) {
+                if (hasWinner()) {
+                    board.print();
+                    System.out.println(currentPlayer.getMarker() + " has won!");
+                    System.out.println("-----------------------------");
+                    start = false;
+                } else if (board.isFull()) {
+                    board.print();
+                    System.out.println("No win! Try again!");
+                    System.out.println("-----------------------------");
+                    start = false;
+                } else {
+                    switchCurrentPlayer();
+                }
+            }
+        }
     }
 
-  public static void main(String[] args) {
-       TicTacToe game = new TicTacToe();
+     public static void main(String[] args) {
+        TicTacToe game = new TicTacToe('X', 'O');
         game.start();
-    }
-
+      }
 }
