8763b665ad1b138c8b825156dfc036ff420665d2
wi23b064 <wi23b064@technikum-wien.at>
2024-07-05 15:52:39 +0200

    place() method implemented

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -2,7 +2,6 @@ package org.example;
 import java.util.Arrays;
 
 public class Board {
-  
     public void place(int x, int y, char marker) {
         if (isCellEmpty(x, y)) {
             cells[x][y] = marker;
