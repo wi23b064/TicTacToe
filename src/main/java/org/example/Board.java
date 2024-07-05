b0153632e93d6ff5d8e6cffce65306619cf179cf
wi23b064 <wi23b064@technikum-wien.at>
2024-07-05 13:39:58 +0200

    Change branches for methods

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -2,7 +2,16 @@ package org.example;
 
 public class Board {
 
-    public void place(int x, int y, char marker){
-        //places marker (x or o) in current position  
+   public boolean checkBoard(char marker) {
+        // Check rows and columns
+        for (int i = 0; i < 3; i++) {
+            if ((cells[i][0] == marker && cells[i][1] == marker && cells[i][2] == marker) ||
+                    (cells[0][i] == marker && cells[1][i] == marker && cells[2][i] == marker)) {
+                return true;
+            }
+        }
+        // Check diagonals
+        return (cells[0][0] == marker && cells[1][1] == marker && cells[2][2] == marker) ||
+                (cells[0][2] == marker && cells[1][1] == marker && cells[2][0] == marker);
     }
 }
