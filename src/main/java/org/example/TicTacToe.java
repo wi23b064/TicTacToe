95352152ff726fd6bd48a022d8c9f840fb19d4dd
Selmaazzam <wi23b091@technikum-wien.at>
2024-07-05 15:19:52 +0200

    Revert "Makeove"

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -3,30 +3,26 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
-    public void start() {
-        boolean start = true;
-        while (start) {
-            board.print();
-            boolean move = makeMove(currentPlayer);
-            if (move) {
-                if (hasWinner()) {
-                    board.print();
-                    System.out.println(currentPlayer.getMarker() + " has won!");
-                    System.out.println("-----------------------------");
-                    start = false;
-                } else if (board.isFull()) {
-                    board.print();
-                    System.out.println("No win! Try again!");
-                    System.out.println("-----------------------------");
-                    start = false;
-                } else {
-                    switchCurrentPlayer();
-                }
-            }
-      }
+    private final Player player1;
+    private final Player player2;
+    private Player currentPlayer;
+    private final Board board;
+    private final Scanner scanner;
+    
+    public TicTacToe(char marker1, char marker2) {
+        player1 = new Player(marker1);
+        player2 = new Player(marker2);
+        currentPlayer = player1;
+        board = new Board();
+        scanner = new Scanner(System.in);
+        
+    }
+    public void switchCurrentPlayer(){
+        currentPlayer = (currentPlayer == player1) ? player2 : player1;
+    }
 
-     public static void main(String[] args) {
-        TicTacToe game = new TicTacToe('X', 'O');
-        game.start();
-      }
+    private boolean hasWinner() {
+        return board.checkBoard(currentPlayer.getMarker());
+    }
+
 }
