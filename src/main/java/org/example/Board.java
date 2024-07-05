dec76785fcacd8455c2754ddd89fabe955b92274
wi23b064 <wi23b064@technikum-wien.at>
2024-07-05 13:37:56 +0200

    Update Board.java

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -30,19 +30,6 @@ public class Board {
             Arrays.fill(cell, '*');
         }
     }
-
-      public boolean checkBoard(char marker) {
-            // Check rows and columns
-            for (int i = 0; i < 3; i++) {
-                if ((cells[i][0] == marker && cells[i][1] == marker && cells[i][2] == marker) ||
-                        (cells[0][i] == marker && cells[1][i] == marker && cells[2][i] == marker)) {
-                    return true;
-                }
-            }
-            // Check diagonals
-            return (cells[0][0] == marker && cells[1][1] == marker && cells[2][2] == marker) ||
-                (cells[0][2] == marker && cells[1][1] == marker && cells[2][0] == marker);
-        }
   
     public void print() {
         for (char[] row : cells) {
