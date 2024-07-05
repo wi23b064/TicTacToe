500c16179526ff43905d3134298a525620cafa2f
Selmaazzam <wi23b091@technikum-wien.at>
2024-07-05 14:30:55 +0200

    Update TicTacToe in branch EndGame.java

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -1,25 +1,9 @@
 package org.example;
 
 public class TicTacToe {
-
-    private Player player1;
-    private Player player2;
-    private Player currentPlayer;
-    private Board board;
-
-    public TicTacToe() {
-        
-    }
-
-    public void start() {
-       
-    }
  
     private boolean hasWinner() {
                return board.checkBoard(currentPlayer.getMarker());
     }
 
-  public static void main(String[] args) {
-      
-    }
 }
