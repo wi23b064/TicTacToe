63988fa9b38b8b9af3df9c1e6e3b6ab1120827c0
wi23b064 <wi23b064@technikum-wien.at>
2024-07-09 14:12:48 +0200

    Update ReStart branch.java

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -2,28 +2,6 @@ package org.example;
 import java.util.Scanner;
 
 public class TicTacToe {
-
-    private final Player player1;
-    private final Player player2;
-    private Player currentPlayer;
-    private final Board board;
-    private final Scanner scanner;
-    
-    public TicTacToe(char marker1, char marker2) {
-        player1 = new Player(marker1);
-        player2 = new Player(marker2);
-        currentPlayer = player1;
-        board = new Board();
-        scanner = new Scanner(System.in);
-        
-    }
-    public void switchCurrentPlayer(){
-        currentPlayer = (currentPlayer == player1) ? player2 : player1;
-    }
-
-    private boolean hasWinner() {
-        return board.checkBoard(currentPlayer.getMarker());
-    }
   
   private boolean restartGame() {
         System.out.println("Do you want to start a new game? \nPress 1 for yes, 0 for no: ");
@@ -43,8 +21,6 @@ public class TicTacToe {
 
 
     public static void main(String[] args) {
-        TicTacToe game = new TicTacToe('X', 'O');
-        game.start();
         if(game.restartGame())
         {
             game.board.clear();
