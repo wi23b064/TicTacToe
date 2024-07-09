06e237a96d2c2e9add23ae3f0079b4f8592742fb
ts-technikum <wi23b160@technikum-wien.at>
2024-07-09 13:23:14 +0200

    Update methods to match

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -3,32 +3,28 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
-    public TicTacToe(char marker1, char marker2) {
-        player1 = new Player(marker1);
-        player2 = new Player(marker2);
-        //new ^^
+  public TicTacToe(char playerOneMarker, char playerTwoMarker) {
         board = new Board();
-        currentPlayer = player1;
+        player1 = new Player(playerOneMarker);
+        player2 = new Player(playerTwoMarker);
+        currentPlayer = player1; // starts
         scanner = new Scanner(System.in);
-        //new ^^
     }
 
-    public void start() {
-        boolean start = true;
-        while (start) {
+   public void start() {
+        boolean gameIsRunning = true;
+        while (gameIsRunning) {
             board.print();
             boolean moveMade = makeMove(currentPlayer);
             if (moveMade) {
                 if (hasWinner()) {
                     board.print();
-                    System.out.println(currentPlayer.getMarker() + " has won!");
-                    System.out.println("-----------------------------");
-                    start = false;
+                    System.out.println(currentPlayer.getMarker() + " wins!");
+                    gameIsRunning = false;
                 } else if (board.isFull()) {
                     board.print();
-                    System.out.println("No win! Try again!");
-                    System.out.println("-----------------------------");
-                    start = false;
+                    System.out.println("It's a draw!");
+                    gameIsRunning = false;
                 } else {
                     switchCurrentPlayer();
                 }
@@ -36,29 +32,31 @@ public class TicTacToe {
         }
     }
 
-    private boolean makeMove(Player player){
-        System.out.println( player.getMarker() + " has to enter a unique position (from 0-2): ");
+   private boolean makeMove(Player player) {
+        System.out.println(player.getMarker() + ", enter the position you want your marker in:");
         int row = scanner.nextInt();
         int col = scanner.nextInt();
         if (row < 0 || row >= 3 || col < 0 || col >= 3) {
-            System.out.println("Error! Try again!");
-            System.out.println("-----------------------------");
-
+            System.out.println("Invalid input. Please try again.");
             return false;
         } else if (!board.isCellEmpty(row, col)) {
-            System.out.println("Occupied! Try another!");
-            System.out.println("-----------------------------");
-
+            System.out.println("This cell is already taken. Please try again.");
             return false;
         } else {
             board.place(row, col, player.getMarker());
             return true;
         }
+
     }
 
     public static void main(String[] args) {
+
         TicTacToe game = new TicTacToe('X', 'O');
         game.start();
-        
-
+        if(game.restartGame())
+        {
+            game.board.clear();
+            game.start();
+        }
+    }
 }
